6d9fcd3c36f8c3e9cb84bd8a9a0d60d83777391c
Busby10 <cmelican@gmail.com>
2017-01-29 18:52:16 +0000

    added g function

--- learntris.py
+++ learntris.py
@@ -7,11 +7,21 @@ play_grid = [[".",".",".",".",".",".",".",".",".","."] for x in range(22)]
 
 while True:
     command = input()
+
     if command == "q":
         exit()
+
     elif command == "p":
         for i,row in enumerate(play_grid):
             print(" ".join(row))
+
+    elif command == "g":
+
+        for y in range(22):
+            row = input()
+            for x,i in enumerate(row.split(" ")):
+                play_grid[y][x] = i
+
 
 
 
